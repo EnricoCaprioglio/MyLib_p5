36c176a403bbfff891f6636cc6b58080951c13ba
Burromarmellata <enricocaprioglio@yahoo.com>
2021-11-02 20:26:20 +0000

    Update mitosis_simulation.js

--- current_projects/mitosis_simulation/mitosis_simulation.js
+++ current_projects/mitosis_simulation/mitosis_simulation.js
@@ -2,14 +2,12 @@ var cells =[];
 
 function setup() {
   createCanvas(600,600);
-  // cell = new Cell(); // then push cell
-  cells.push(new Cell());
+  cells.push(new Cell()); // same as: cell = new Cell(); then push cell
   cells.push(new Cell());
 }
 
-
 function draw() {
-  background(51);
+  background(38);
   for (var i = 0; i < cells.length; i++) {
     cells[i].move();
     cells[i].show();
@@ -19,8 +17,11 @@ function draw() {
 function mousePressed() {
   for (var i = cells.length - 1; i >= 0; i--) {
     if (cells[i].clicked(mouseX, mouseY)) {
-      cells.push(cells[i].mitosis());
-      cells.push(cells[i].mitosis());
+      // cells.push(cells[i].mitosis());
+      // cells.push(cells[i].mitosis());
+      var offset = createVector(random(-cells[i].r, cells[i].r),random(-cells[i].r, cells[i].r));
+      cells.push(new Cell(cells[i].pos.add(offset), cells[i].r*0.8, cells[i].c))
+      cells.push(new Cell(cells[i].pos.add(-offset), cells[i].r*0.8, cells[i].c))
       cells.splice(i, 1);
     }   
   }
