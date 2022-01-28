413f72e94a662fd42096bcbf98228aede69e4a9a
Burromarmellata <enricocaprioglio@yahoo.com>
2022-01-28 13:12:39 +0000

    Update mitosis_simulation.js

--- current_projects/mitosis_simulation/mitosis_simulation.js
+++ current_projects/mitosis_simulation/mitosis_simulation.js
@@ -2,8 +2,9 @@ var cells =[];
 
 function setup() {
   createCanvas(600,600);
-  cells.push(new Cell()); // same as: cell = new Cell(); then push cell
-  cells.push(new Cell());
+  for (var i = 0; i < 2; i++) {
+    cells.push(new Cell());
+  }
 }
 
 function draw() {
@@ -11,18 +12,25 @@ function draw() {
   for (var i = 0; i < cells.length; i++) {
     cells[i].move();
     cells[i].show();
+    cells[i].edges();
   }
 }
 
 function mousePressed() {
+  angleMode(DEGREES)
   for (var i = cells.length - 1; i >= 0; i--) {
     if (cells[i].clicked(mouseX, mouseY)) {
-      // cells.push(cells[i].mitosis());
-      // cells.push(cells[i].mitosis());
-      var offset = createVector(random(-cells[i].r, cells[i].r),random(-cells[i].r, cells[i].r));
-      cells.push(new Cell(cells[i].pos.add(offset), cells[i].r*0.8, cells[i].c))
-      cells.push(new Cell(cells[i].pos.add(-offset), cells[i].r*0.8, cells[i].c))
+      var temp_pos = createVector(cells[i].pos.x+0.1, cells[i].pos.y+0.1);
+      var temp_r = cells[i].r*0.8;
+      var temp_c = cells[i].c;
+      var temp_vel = cells[i].vel.copy();
+      var perp_acc1 = createVector(-temp_vel.y, temp_vel.x);
+      var perp_acc2 = perp_acc1.copy().mult(-1);
+      cells.push(new Cell(temp_pos, temp_r, temp_c, temp_vel.copy().add(perp_acc1)));
+      cells.push(new Cell(temp_pos, temp_r, temp_c, temp_vel.copy().add(perp_acc2)));
       cells.splice(i, 1);
-    }   
+      // console.log("this is mag", mag(cells[i].vel.x, cells[i].vel.y));
+      // console.log("vel ", cells[i].vel.x, cells[i].vel.y);
+    }
   }
 }
